ce04ff8d1c0d2dd77c5be9b7c2359302b567efca
Ricardo F. Rodrigues <158061450+ricardodev28@users.noreply.github.com>
2024-09-20 15:09:02 -0300

    Add files via upload

---
+++ src/components/cardnews.js
@@ -0,0 +1,10 @@
+class cardnews extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({mode: "open"})
+        shadow.innerHTML = "<h1> Hello World</h1>";
+        }
+}
+
+customElements.define('card-news',cardnews);
